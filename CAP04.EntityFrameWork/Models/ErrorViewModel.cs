557b5f88d6e8e1a08d5a6b0dffe67358033d33ac
unknown <lmacumot@trf3.jus.br>
2022-11-21 23:29:02 -0300

    first commit

---
+++ CAP04.EntityFrameWork/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CAP04.EntityFrameWork.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ CAP04.EntityFrameWork/Program.cs
@@ -0,0 +1,41 @@
+
+
+global using CAP04.EntityFrameWork.Data;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.AspNetCore.Mvc;
+global using System.Diagnostics;
+//using CAP04.EntityFrameWork.Data;
+//using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+//builder.Services.AddDbContext<NorthwindContext>(x => x.UseSqlServer("Data Source=.\\SQLEXPRESS; Initial catalog=NorthWind; trusted_connection=yes"));
+builder.Services.AddDbContext<NorthwindContext>(x => x.UseSqlServer("Data Source=.\\localhost; Initial catalog=Northwind; trusted_connection=yes"));
+
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
